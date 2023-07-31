e93c840c036bc62c706257b87004dce575da5095
Ruslan <kovalyovruslan@gmail.com>
2023-07-31 08:16:36 +0300

    robots.txt

--- sushibook/urls.py
+++ sushibook/urls.py
@@ -23,3 +23,14 @@ if DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'main.views.Oops'
+
+
+#add robots.txt
+from django.views.static import serve
+from django.urls import re_path
+
+urlpatterns += [
+    re_path(r'^robots\.txt$', serve, {'document_root': settings.BASE_DIR, 'path': "/robots.txt"}),
+]
+
+
